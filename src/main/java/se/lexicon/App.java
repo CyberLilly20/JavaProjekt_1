30831e1663628cbe1a5d06e18bc8209948d8b9b1
liljana ristevska <li.ristevska@gmail.com>
2022-10-11 15:09:24 +0200

    exersize2

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -2,12 +2,26 @@ package se.lexicon;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello");
+        System.out.println("Liljana!");
+
+        // if els if statement
+
+
+        int year = 2800;
+        if (year % 400 == 0) {
+            System.out.println("Leap year");
+        } else if (year % 100 == 0) {
+            System.out.println("Not leap year");
+        } else if (year % 4 == 0) {
+            System.out.println("Leap year");
+        }
+
     }
+
+
 }

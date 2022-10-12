917f5313ce85d7d2b65f581b8cc9273aa91bae4f
liljana ristevska <li.ristevska@gmail.com>
2022-10-12 13:45:46 +0200

    exercise3

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -21,8 +21,58 @@ public class App {
             System.out.println("Leap year");
 
         }
+        {
+
+          // arithmetic operations
+            int num1 = 45;
+            int num2 = 11;
+
+            int sum = num1 + num2;
+            {
+                System.out.println( num1 +"+" + num2 + "=" +sum);
+            }
+            {
+            int num3 = 12;
+            int num4 = 4;
+            System.out.println( num3+ "*" +num4+ "="+( num3*num4));
+            }
+
+           int num5 = 24;
+           int num6 = 6;
+           System.out.println(num5+ "/" +num6+ "=" + (num5/num6));
+
+
+           int num7 = 55;
+           int num8 = 12;
+           System.out.println(num7+ "-" + num8 + "=" +(num7-num8));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
 
     }
+
+
+
+
+
+
+
+
 
 
 }
